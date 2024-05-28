451ab64a1f428a5fb2f84be80eef1446745a7d9d
igor ntwali <82104389+igorntwari@users.noreply.github.com>
2024-05-28 10:54:24 +0200

    feat:initilised initial files

---
+++ test/firstunit.js
@@ -0,0 +1,5 @@
+const sinon = require('sinon')
+const chai = require('chai')
+const expect = chai.expect
+const swapi = require('../apis/swapi.js')
+const starwars = require('../controllers/starwars.js')
